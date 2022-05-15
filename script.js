ce4ac0cf2e7485566e54635b01ad511043b2f10f
Luis Antonio <inluisantonio2.2@gmail.com>
2022-05-15 14:31:11 -0500

    V5
    Mostrar resultados dinamicos en una lista

--- script.js
+++ script.js
@@ -11,22 +11,25 @@ async function cargarUrl(url){
 }
 
 async function clickBoton(){   
-    let json = await cargarUrl('https://api.covid19tracking.narrativa.com/api/2020-05-05/country/spain');
+    let pais = document.getElementById('selectPais').value;
+    let fecha = document.getElementById('inputFecha').value;
+    
+    let url = `https://api.covid19tracking.narrativa.com/api/${fecha}/country/${pais}`
+    let json = await cargarUrl(url);
     //console.log(json);
 
-    let estadisticas = json.dates['2020-05-05'].countries.Spain;
-    console.log(estadisticas);
-
-
-    today_confirmed: 221458
-    today_deaths: 25854
-    today_hospitalised_patients_with_symptoms: 0
-    today_intensive_care: 11059
-    today_new_confirmed: 873
-    today_new_deaths: 244
-    today_new_hospitalised_patients_with_symptoms: 0
-    today_new_intensive_care: 51
-    today_new_open_cases: -1887
-    today_new_recovered: 2516
-    today_new_total_hospitalised_patients: 829
+    let estadisticas = json.dates[fecha].countries[pais];
+    //console.log(estadisticas);
+   
+    document.getElementById('today_confirmed').innerHTML = estadisticas.today_confirmed;
+    document.getElementById('today_deaths').innerHTML = estadisticas.today_deaths;
+    document.getElementById('today_hospitalised_patients_with_symptoms').innerHTML = estadisticas.today_hospitalised_patients_with_symptoms;
+    document.getElementById('today_intensive_care').innerHTML = estadisticas.today_intensive_care;
+    document.getElementById('today_new_confirmed').innerHTML = estadisticas.today_new_confirmed;
+    document.getElementById('today_new_deaths').innerHTML = estadisticas.today_new_deaths;
+    document.getElementById('today_new_hospitalised_patients_with_symptoms').innerHTML = estadisticas.today_new_hospitalised_patients_with_symptoms;
+    document.getElementById('today_new_intensive_care').innerHTML = estadisticas.today_new_intensive_care;
+    document.getElementById('today_new_open_cases').innerHTML = estadisticas.today_new_open_cases;
+    document.getElementById('today_new_recovered').innerHTML = estadisticas.today_new_recovered;
+    document.getElementById('today_new_total_hospitalised_patients').innerHTML = estadisticas.today_new_total_hospitalised_patients;
 }
